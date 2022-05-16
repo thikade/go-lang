e42aa1ab90d352d8faee3dee3fea800634f59499
thikade <thomas.hikade@gmail.com>
2022-05-16 16:52:33 +0200

    serving a template

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"embed"
 	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
+	"text/template"
 	"time"
 
 	"github.com/gorilla/mux"
 )
 
-// docRoot holds our static
+// fsContent holds our static
 //go:embed public/*
-// //go:embed templates/*
-var docRoot embed.FS
+//go:embed templates/*
+var fsContent embed.FS
+
+var pets = []struct {
+	Animal string
+	Age    int
+}{
+	{
+		Animal: "Cat",
+		Age:    3,
+	},
+	{
+		Animal: "Dog",
+		Age:    7,
+	},
+}
 
 func main() {
 	router := mux.NewRouter()
@@ -27,11 +43,12 @@ func main() {
 		Handler:        router,
 	}
 
-	publicFS, err := fs.Sub(docRoot, "public")
+	publicFS, err := fs.Sub(fsContent, "public")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// https://gowebexamples.com/routes-using-gorilla-mux/
 	router.HandleFunc("/customer/{id:[-a-zA-Z_0-9.]+}", func(w http.ResponseWriter, r *http.Request) {
 		v := mux.Vars(r)
 		id := v["id"]
@@ -42,10 +59,25 @@ func main() {
 		}
 	})
 
+	router.HandleFunc("/render", func(w http.ResponseWriter, r *http.Request) {
+		templateFile, err := fsContent.ReadFile("templates/demo1.tpl")
+		if err != nil {
+			log.Fatal(err)
+		}
+		t := template.Must(template.New("table").Parse(string(templateFile)))
+
+		buffer := bytes.Buffer{}
+		err = t.Execute(&buffer, pets)
+		if err != nil {
+			panic(err)
+		}
+		w.Write(buffer.Bytes())
+
+	})
+
 	// default server from FS
 	router.PathPrefix("/").Handler(http.FileServer(http.FS(publicFS)))
 
 	log.Printf("Listening on port: %d\n", port)
 	log.Fatal(s.ListenAndServe())
-
 }
---
+++ templates/demo1.tpl
@@ -0,0 +1,39 @@
+<!doctype html> 
+<htmllang="en"> 
+<head> 
+   <meta charset="UTF-8"> 
+   <title>Using Go HTML Templates</title> 
+   <style> 
+         html { 
+               font-size: 16px; 
+         } 
+         table, th, td { 
+         border: 3px solid gray; 
+         } 
+   </style> 
+</head> 
+<body> 
+ 
+<h2 alight="center">Presenting Dynamic content!</h2> 
+ 
+<table> 
+   <thead> 
+         <tr> 
+               <th>Animal</th> 
+               <th>Age</th> 
+               <th>?</th> 
+         </tr> 
+   </thead> 
+   <tbody> 
+{{ range . }} 
+<tr> 
+   <td> {{ .Animal }} </td> 
+   <td> {{ .Age }} </td> 
+   <td> {{ .Age }} </td> 
+</tr> 
+{{ end }} 
+   </tbody> 
+</table> 
+ 
+</body> 
+</html> 
