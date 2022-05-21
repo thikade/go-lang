28b208c4ea93aef46cfd9fc0003bab0957638637
thikade <thomas.hikade@gmail.com>
2022-05-21 14:05:42 +0200

    add central  error functions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log"
 	"net/http"
 	"text/template"
 
@@ -17,14 +16,16 @@ import (
 func (app *application) render(w http.ResponseWriter, filename string, data interface{}) {
 	tmpl, err := template.ParseFiles(filename)
 	if err != nil {
-		app.errorLog.Println(err)
-		http.Error(w, "Sorry, something went wrong", http.StatusInternalServerError)
+		app.serverError(w, err)
+		// app.errorLog.Println(err)
+		// http.Error(w, "Sorry, something went wrong", http.StatusInternalServerError)
 		return
 	}
 
 	if err := tmpl.Execute(w, data); err != nil {
-		app.errorLog.Println(err)
-		http.Error(w, "Sorry, something went wrong in Template rendering ", http.StatusInternalServerError)
+		app.serverError(w, err)
+		// app.errorLog.Println(err)
+		// http.Error(w, "Sorry, something went wrong in Template rendering ", http.StatusInternalServerError)
 	}
 }
 
@@ -47,8 +48,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// paths as a variadic parameter?
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) // Use the serverError() helper
 		return
 	}
 
@@ -56,8 +56,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// template as the response body.
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) // Use the serverError() helper	}
 	}
 }
 
@@ -70,7 +69,8 @@ func (app *application) customer(w http.ResponseWriter, r *http.Request) {
 	if id == "alex" {
 		w.Write([]byte(fmt.Sprintf("Found customer: %s\n", id)))
 	} else {
-		http.Error(w, fmt.Sprintf("Customer %s not found", id), http.StatusTeapot)
+		app.notFound(w)
+		// http.Error(w, fmt.Sprintf("Customer %s not found", id), http.StatusTeapot)
 	}
 }
 
@@ -80,8 +80,9 @@ func (app *application) customer(w http.ResponseWriter, r *http.Request) {
 func (app *application) renderEmbeddedFile(w http.ResponseWriter, r *http.Request) {
 	templateFile_demo1, err := ui.EfsFiles.ReadFile("html/embedded.tpl")
 	if err != nil {
-		app.errorLog.Println(err)
-		http.Error(w, fmt.Sprintf("404: template not found: %s", "html/embedded.tpl"), http.StatusNotFound)
+		// app.errorLog.Println(err)
+		// http.Error(w, fmt.Sprintf("404: template not found: %s", "html/embedded.tpl"), http.StatusNotFound)
+		app.notFound(w)
 		return
 	}
 	t_demo1 := template.Must(template.New("table").Parse(string(templateFile_demo1)))
@@ -89,7 +90,8 @@ func (app *application) renderEmbeddedFile(w http.ResponseWriter, r *http.Reques
 	buffer := bytes.Buffer{}
 	err = t_demo1.Execute(&buffer, pets)
 	if err != nil {
-		panic(err)
+		// panic(err)
+		app.serverError(w, err)
 	}
 	w.Write(buffer.Bytes())
 }
@@ -103,13 +105,15 @@ func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request) {
 
 	myT, err := template.ParseGlob(fmt.Sprintf("ui/html/external_%s.tpl", id))
 	if err != nil {
-		app.errorLog.Println("template not found!")
-		http.Error(w, fmt.Sprintf("404: template not found: external_%s.tpl", id), http.StatusNotFound)
+		// app.errorLog.Println("template not found!")
+		// http.Error(w, fmt.Sprintf("404: template not found: external_%s.tpl", id), http.StatusNotFound)
+		app.notFound(w)
 		return
 	}
 	if err := myT.Execute(w, pets); err != nil {
-		app.errorLog.Println(err)
-		http.Error(w, "Sorry, something went wrong in Template rendering ", http.StatusInternalServerError)
+		// app.errorLog.Println(err)
+		// http.Error(w, "Sorry, something went wrong in Template rendering ", http.StatusInternalServerError)
+		app.serverError(w, err)
 	}
 }
 
